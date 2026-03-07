dec6ad84d4890e075db1bc212e153bbaae3c5067
Uzair Ahmad <uzairahmadxy@gmail.com>
2026-03-07 06:02:50 -0500

    Initial Commit

---
+++ TypeAhead/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TypeAhead
+//
+//  Created by Uzair on 2026-03-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TypeAhead/TypeAheadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeAheadApp.swift
+//  TypeAhead
+//
+//  Created by Uzair on 2026-03-07.
+//
+
+import SwiftUI
+
+@main
+struct TypeAheadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
